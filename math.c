dd97efcda7e570339a59a525c195fd501823ff77
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-01-18 23:09:15 +0300

    maths

--- math.c
+++ math.c
@@ -57,31 +57,8 @@ void m_div(stack_t **stack, unsigned int line_number)
 	*stack = temp1;
 	free(temp);
 }
- /**
- * m_mul - multiplies the second top element of the stack
- * with the top element of the stack.
- * @stack: pointer of pointer to actual stack
- * @line_number: file line number
- *
- */
-void m_mul(stack_t **stack, unsigned int line_number);
-{
-	stack_t *temp = *stack, *temp1;
 
-	if ((*stack) == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
-		all_free();
-		frees(*stack);
-		exit(EXIT_FAILURE);
-	}
-	temp1 = temp->next;
-	temp1->n *= temp->n;
-	temp1->prev = NULL;
-	*stack = temp1;
-	free(temp);
-}
-}
+
 /**
  * m_mul - multiplies the second top element of the stack
  * with the top element of the stack.
